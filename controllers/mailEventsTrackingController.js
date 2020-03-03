9292678468712db5d0e6169db733a040a9cbcccb
rob_ <robertvrenozi@thedancingbist.com>
2020-03-03 17:05:13 +0100

    Link Click event accepts parameters encoded in base64 format, this in order to make the process obscure to the user

--- controllers/mailEventsTrackingController.js
+++ controllers/mailEventsTrackingController.js
@@ -2,24 +2,28 @@ let mailRepository = require('../repository/repository');
 
 let mailEventsTrackerCtrl = {
   mailOpenedEventTracker: (req, res) => {
-   mailRepository.checkTrackingExistence(req.query)
-     .then(data => {
-       if (data) {
-         // Mail Track already exists => update it
-         data.numberOfViews++;
-         data.lastSeenActivity = new Date();
-         mailRepository.updateEmailTrackRecord(data).then(res.send(''));
-       } else {
-         // Mail Track does not exist => create new one
-         mailRepository.saveEmailTrackRecord({
-           senderId: req.query.senderId,
-           receiverEmail: req.query.receiverEmail,
-           campaignId: req.query.campaignId
-         }).then(res.send(''));
-       }
-     });
+    if (!(req.query.receiverEmail && req.query.senderId && req.query.campaignId)) {
+      res.status(400);
+      res.send('NOK');
+    } else {
+      mailRepository.checkTrackingExistence(req.query)
+        .then(data => {
+          if (data) {
+            // Mail Track already exists => update it
+            data['numberOfViews']++;
+            data['lastSeenActivity'] = new Date();
+            mailRepository.updateEmailTrackRecord(data).then(res.send(''));
+          } else {
+            // Mail Track does not exist => create new one
+            mailRepository.saveEmailTrackRecord({
+              senderId: req.query.senderId,
+              receiverEmail: req.query.receiverEmail,
+              campaignId: req.query.campaignId
+            }).then(res.send(''));
+          }
+        });
+    }
   },
-
   getMailOpenedEventTracker: (req, res) => {
     mailRepository.getAllMailOpenedData()
       .then(data => {
@@ -28,7 +32,59 @@ let mailEventsTrackerCtrl = {
         console.log(e);
         res.send(e);
       });
-   }
+  },
+  linkClickedTracker: (req, res) => {
+    if (!req.query.info) {
+      res.status(400);
+      res.send('NOK');
+    } else {
+      let buff = new Buffer.from(req.query.info, 'base64');
+      let text = buff.toString('ascii');
+      let parameters = getUrlParametersInObj(text);
+
+      if (!(parameters.receiverEmail && parameters.senderId && parameters.campaignId && parameters.linkId && parameters.url)) {
+        res.status(400);
+        res.send('NOK');
+      } else {
+        mailRepository.checkLinkClickedExistence(parameters)
+          .then(data => {
+            if (data) {
+              // Link clicked exists => update data
+              data['numberOfClicks']++;
+              mailRepository.updateLinkClickedRecord(data).then(() => {
+                redirectToUrl(res, parameters.url);
+              });
+            } else {
+              // New link click
+              mailRepository.saveLinkClickedRecord({
+                senderId: parameters.senderId,
+                receiverEmail: parameters.receiverEmail,
+                campaignId: parameters.campaignId,
+                linkId: parameters.linkId
+              }).then(() => {
+                redirectToUrl(res, parameters.url);
+              });
+            }
+          });
+      }
+    }
+  }
+};
+
+let redirectToUrl = (res, url) => {
+  res.writeHead(301, { Location: url });
+  res.end();
+};
+
+let getUrlParametersInObj = (urlParams) => {
+  let parametersArray = urlParams.split('&');
+  let temp, urlParameters = {};
+  parametersArray.forEach(function(el) {
+    temp = el.split('=');
+    urlParameters[temp[0]] = temp[1];
+  });
+
+  return urlParameters;
 };
 
 module.exports = mailEventsTrackerCtrl;
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,9 @@ let router = express.Router();
 let mailEventsTrackingCtrl = require('../controllers/mailEventsTrackingController');
 
 let mainRouter = () => {
-  router.get('/', mailEventsTrackingCtrl.getMailOpenedEventTracker );
-  router.get('/opened', mailEventsTrackingCtrl.mailOpenedEventTracker); 
-
-  router.get('/linkClicked', (req, res) => {
-    res.send('Link Clicked');
-  });
+  router.get('/', mailEventsTrackingCtrl.getMailOpenedEventTracker);
+  router.get('/opened', mailEventsTrackingCtrl.mailOpenedEventTracker);
+  router.get('/linkClicked', mailEventsTrackingCtrl.linkClickedTracker);
 
   return router;
 };
